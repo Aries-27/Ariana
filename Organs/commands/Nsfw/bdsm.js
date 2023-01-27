25ffb743f71d83ac06d42cde1243f0e7e33be4a1
Aries-27 <101610365+Aries-27@users.noreply.github.com>
2023-01-27 17:32:16 +0530

    Update bdsm.js

--- Organs/commands/Nsfw/bdsm.js
+++ Organs/commands/Nsfw/bdsm.js
@@ -21,7 +21,7 @@ module.exports={
     let buttonMessage = {
         image: {url:ass},
         caption: `*⛓️*`,
-        footer: `*©Eternity-Team*`,
+        footer: `*©Ronen-Bots*`,
         buttons: buttons,
         headerType: 4
     }
