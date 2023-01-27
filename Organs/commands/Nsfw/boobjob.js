9bda432803e986a1433bc2d7afab64cf66e50a65
Aries-27 <101610365+Aries-27@users.noreply.github.com>
2023-01-27 17:33:15 +0530

    Update boobjob.js

--- Organs/commands/Nsfw/boobjob.js
+++ Organs/commands/Nsfw/boobjob.js
@@ -21,7 +21,7 @@ module.exports={
     let buttonMessage = {
         image: {url:ass},
         caption: `*😈*`,
-        footer: `*©Eternity-Team*`,
+        footer: `*©Ronen-Bots*`,
         buttons: buttons,
         headerType: 4
     }
