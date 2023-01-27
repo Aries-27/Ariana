bd678d1ce8672331a26a153c5df53839498bc143
Aries-27 <101610365+Aries-27@users.noreply.github.com>
2023-01-27 16:02:48 +0530

    Update hi.js

--- Organs/commands/General/hi.js
+++ Organs/commands/General/hi.js
@@ -16,4 +16,4 @@ module.exports = {
         await client.sendMessage(m.from,{text:`Heyaa! ${pushName}\nHere are the rules of Pikachu Bot. Follow the rules properly to avoid getting block or ban\n\n_*----🎀[Rule]🎀----*_\n\n❌ *RULES SHOULD BE FOLLOWED STRICTLY* 🚫\n*>>>* use *.support* to get the Official group link in your dm\n*--->* Don't chat in private with bot\n*--->* If you want to add Pikachu Bot in your group then contact the mod by using *.mod* \n*--->* Dont use wrong command, use the command given in the *help list* \n*--->* Dont spam the bot with commands if the bot is not responding, it means the bot is maybe offline or facing internet issue. \n\n*IF YOU DONT FOLLOW THE RULES THEN YOU WILL BE BAN SOON* 🚫\n\n\n*~Regards- Ronen*`},{quoted:m})
         }
     }
-}s
+}
