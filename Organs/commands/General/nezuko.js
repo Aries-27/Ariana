08248109d8d4dc0fcec3769f11f85f1b46e956d3
Aries-27 <101610365+Aries-27@users.noreply.github.com>
2023-01-27 14:26:17 +0530

    Update nezuko.js

--- Organs/commands/General/nezuko.js
+++ Organs/commands/General/nezuko.js
@@ -15,12 +15,12 @@ module.exports = {
         args
     }) => {
 let yup = [
-            'https://images6.alphacoders.com/103/1037400.png',
-            'https://images3.alphacoders.com/110/1105901.jpg',
-            'https://images5.alphacoders.com/102/1028689.jpg',
-            'https://images4.alphacoders.com/102/1029828.png',
-            'https://wallpaperaccess.com/full/1105099.jpg',
-            'https://images8.alphacoders.com/120/1206602.png'
+            'https://i.ibb.co/x78n9mC/20230127-141609.jpg',
+            'https://i.ibb.co/x78n9mC/20230127-141609.jpg',
+            'https://i.ibb.co/x78n9mC/20230127-141609.jpg',
+            'https://i.ibb.co/x78n9mC/20230127-141609.jpg',
+            'https://i.ibb.co/x78n9mC/20230127-141609.jpg',
+            'https://i.ibb.co/x78n9mC/20230127-141609.jpg'
           ]
 let nezuko = await axios.get('https://api.github.com/repos/EternityBots/Nezuko')
 let rae = yup[Math.floor(Math.random() * yup.length)]
