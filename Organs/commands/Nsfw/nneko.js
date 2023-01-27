237d2b35d326d001e64c4dee3590d6ff71869c90
Aries-27 <101610365+Aries-27@users.noreply.github.com>
2023-01-27 17:34:34 +0530

    Update nneko.js

--- Organs/commands/Nsfw/nneko.js
+++ Organs/commands/Nsfw/nneko.js
@@ -25,7 +25,7 @@ module.exports = {
     let buttonMessage = {
       image: { url: wife.data.url },
       caption: `*🥵 NYAAAAAAA*`,
-      footer: `*©Eternity-Team*`,
+      footer: `*©Ronen-Bots*`,
       buttons: buttons,
       headerType: 4,
     };
