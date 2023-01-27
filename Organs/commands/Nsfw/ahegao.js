33447e3533d4e6e509f2601e294c9142f57bb5aa
Aries-27 <101610365+Aries-27@users.noreply.github.com>
2023-01-27 17:31:20 +0530

    Update ahegao.js

--- Organs/commands/Nsfw/ahegao.js
+++ Organs/commands/Nsfw/ahegao.js
@@ -21,7 +21,7 @@ module.exports={
     let buttonMessage = {
         image: {url:ass},
         caption: `*🥵*`,
-        footer: `*©Eternity-Team*`,
+        footer: `*©Ronen-Bots*`,
         buttons: buttons,
         headerType: 4
     }
