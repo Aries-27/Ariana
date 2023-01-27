7aaa996aeba8272a78bfe533a44c25ae2da98c16
Aries-27 <101610365+Aries-27@users.noreply.github.com>
2023-01-27 15:57:47 +0530

    Update hi.js

--- Organs/commands/General/hi.js
+++ Organs/commands/General/hi.js
@@ -1,7 +1,7 @@
 module.exports = {
-    name: "hi",
-    alias: ["hello"],
-    desc: "Tells hello",
+    name: "rule",
+    alias: ["r"],
+    desc: "Sends you the list of rules of the Bot",
     cool:3,
     react:"✅",
     category: "General",
@@ -9,11 +9,11 @@ module.exports = {
         if(m.quoted){
             const nme= await client.username(m.quoted.sender)
             console.log(nme)
-            await client.sendMessage(m.from,{text:`Heyaa! ${nme}\nHere are the rules of Pikachu Bot. Follow the rules properly to avoid getting block or ban\n\n_*----🎀[Rule]🎀----*_\n\n❌ *RULES SHOULD BE FOLLOWED STRICTLY* 🚫\n*>>>* use *.support* to get the Official group link in your dm\n*--->* Don't chat in private with bot\n*--->* If you want to add Pikachu Bot in your group then contact the mkd
-            ds \n*--->* Dont use wrong command, use the command given in the *help list* \n*--->* Dont spam the bot with commands if the bot is not responding, it means the bot is maybe offline or facing internet issue. \n*--->* Dont Dm the bot \n\n*IF YOU DONT FOLLOW THE RULES THEN YOU WILL BE BAN SOON* 🚫`},{quoted:m})
+            await client.sendMessage(m.from,{text:`Heyaa! ${nme}\nHere are the rules of Pikachu Bot. Follow the rules properly to avoid getting block or ban\n\n_*----🎀[Rule]🎀----*_\n\n❌ *RULES SHOULD BE FOLLOWED STRICTLY* 🚫\n*>>>* use *.support* to get the Official group link in your dm\n*--->* Don't chat in private with bot\n*--->* If you want to add Pikachu Bot in your group then contact the mod by using *.mod*
+            *--->* Dont use wrong command, use the command given in the *help list* \n*--->* Dont spam the bot with commands if the bot is not responding, it means the bot is maybe offline or facing internet issue. \n\n*IF YOU DONT FOLLOW THE RULES THEN YOU WILL BE BAN SOON* 🚫\n\n\n*~Regards- Ronen*`},{quoted:m})
 
         }else{
-        await client.sendMessage(m.from,{text:`Heyaa! ${pushName}`},{quoted:m})
+        await client.sendMessage(m.from,{text:`Heyaa! ${pushName}\nHere are the rules of Pikachu Bot. Follow the rules properly to avoid getting block or ban\n\n_*----🎀[Rule]🎀----*_\n\n❌ *RULES SHOULD BE FOLLOWED STRICTLY* 🚫\n*>>>* use *.support* to get the Official group link in your dm\n*--->* Don't chat in private with bot\n*--->* If you want to add Pikachu Bot in your group then contact the mod by using *.mod* \n*--->* Dont use wrong command, use the command given in the *help list* \n*--->* Dont spam the bot with commands if the bot is not responding, it means the bot is maybe offline or facing internet issue. \n\n*IF YOU DONT FOLLOW THE RULES THEN YOU WILL BE BAN SOON* 🚫\n\n\n*~Regards- Ronen*`},{quoted:m})
         }
     }
 }s
