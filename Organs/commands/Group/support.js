2e5e3cf138a73795cdf3227f9bc9e54caf8ea359
Aries-27 <101610365+Aries-27@users.noreply.github.com>
2023-01-27 17:04:31 +0530

    Update support.js

--- Organs/commands/Group/support.js
+++ Organs/commands/Group/support.js
@@ -6,10 +6,10 @@ module.exports = {
     react:"✅",
     start:async(client,m,{command,prefix,pushName})=>{
         let yup = ['https://media.tenor.com/rBGm1p-7uNQAAAPo/pikachu-happy.mp4',
-                   'https://media.tenor.com/xBh07rz9GHYAAAPo/nezuko-kamado-nezuko.mp4',
-                   'https://media.tenor.com/CMHP0DIQY4UAAAPo/demon-slayer-kimetsu-no-yaiba.mp4',
-                   'https://media.tenor.com/68c6pxzq3RAAAAPo/nezuko-nezuko-kamado.mp4',
-                   'https://media.tenor.com/JQgNUfM_xcwAAAPo/nezuko-kamado-run.mp4'
+                   'https://media.tenor.com/rBGm1p-7uNQAAAPo/pikachu-happy.mp4',
+                   'https://media.tenor.com/rBGm1p-7uNQAAAPo/pikachu-happy.mp4',
+                   'https://media.tenor.com/qK8IPos5MZkAAAPo/pikachu-pikachucute.mp4',
+                   'https://media.tenor.com/qK8IPos5MZkAAAPo/pikachu-pikachucute.mp4'
                   ]
 let rae = yup[Math.floor(Math.random() * yup.length)]
 
