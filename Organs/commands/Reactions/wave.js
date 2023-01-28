af2c15e61de8433da05ce17369c221ee0556b8c6
Aries-27 <101610365+Aries-27@users.noreply.github.com>
2023-01-28 20:10:41 +0530

    Update wave.js

--- Organs/commands/Reactions/wave.js
+++ Organs/commands/Reactions/wave.js
@@ -1,1 +1,19 @@
+const { buffergif, fetchBuffer } = require('../../../lib/Function')
+const axios = require('axios')
+require ('../../../settings')
+module.exports={
+    name:"wave",
+    alias:["waved"],
+    usage:`${prefa}bj`,
+    desc:"Sends a gif of waving!",
+    category:"Reactions",
+    react:"👋",
+start:async(client,m,{command,nsfw,prefix,text})=>{
+  let assss = await axios.get ("https://api.waifu.pics/nsfw/blowjob")
+    var bobuff = await fetchBuffer(assss.data.url)
+    var bogif = await buffergif(bobuff)
+    await client.sendMessage(m.from,{video:bogif, gifPlayback:true },{quoted:m}).catch(err => {
+    })
+  }
+}
 
