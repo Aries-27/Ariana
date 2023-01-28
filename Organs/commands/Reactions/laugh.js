5a34101b41b86c0129af1aafc38b022cf82d0fab
Aries-27 <101610365+Aries-27@users.noreply.github.com>
2023-01-28 21:11:23 +0530

    Update laugh.js

--- Organs/commands/Reactions/laugh.js
+++ Organs/commands/Reactions/laugh.js
@@ -2,15 +2,15 @@ const { buffergif, fetchBuffer } = require('../../../lib/Function')
 const axios = require('axios')
 require ('../../../settings')
 module.exports={
-    name:"laugh",
-    alias:["laughed"],
-    usage:`${prefa}laugh`,
-    desc:"Sends a gif of laughing!",
+    name:"pat",
+    alias:["patt"],
+    usage:`${prefa}pat`,
+    desc:"Sends a gif of patting!",
     category:"Reactions",
-    react:"🤣",
+    react:"😙",
 start:async(client,m,{command,nsfw,prefix,text})=>{
-  let assss = await axios.get ("https://api.waifu.pics/sfw/laugh")
-  var lol = '*You laughed at them🤣*'
+  let assss = await axios.get ("https://api.waifu.pics/sfw/pat")
+  var lol = '*You patted them😙*'
     var bobuff = await fetchBuffer(assss.data.url)
     var bogif = await buffergif(bobuff)
     await client.sendMessage(m.from,{video:bogif, caption:lol, gifPlayback:true },{quoted:m}).catch(err => {
