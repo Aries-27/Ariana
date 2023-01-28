831a258484016c9ebdadfbb1af9e8fb58f15a5e3
Aries-27 <101610365+Aries-27@users.noreply.github.com>
2023-01-28 20:46:53 +0530

    Create kiss.js

---
+++ Organs/commands/Reactions/kiss.js
@@ -0,0 +1,19 @@
+const { buffergif, fetchBuffer } = require('../../../lib/Function')
+const axios = require('axios')
+require ('../../../settings')
+module.exports={
+    name:"kiss",
+    alias:["kissed"],
+    usage:`${prefa}kiss`,
+    desc:"Sends a gif of kissing!",
+    category:"Reactions",
+    react:"😘",
+start:async(client,m,{command,nsfw,prefix,text})=>{
+  let assss = await axios.get ("https://api.waifu.pics/sfw/wave")
+  var lol = '*You kissed them*'
+    var bobuff = await fetchBuffer(assss.data.url)
+    var bogif = await buffergif(bobuff)
+    await client.sendMessage(m.from,{video:bogif, caption:lol, gifPlayback:true },{quoted:m}).catch(err => {
+    })
+  }
+}
