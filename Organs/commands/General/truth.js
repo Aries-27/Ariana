8591bf1cce94695454cd390be2ba7245a352ac2c
Aries-27 <101610365+Aries-27@users.noreply.github.com>
2023-01-28 16:06:22 +0530

    Update truth.js

--- Organs/commands/General/truth.js
+++ Organs/commands/General/truth.js
@@ -22,7 +22,7 @@ let yup = [
 let nezuko = await axios.get('https://api.github.com/repos/EternityBots/Nezuko')
 let rae = yup[Math.floor(Math.random() * yup.length)]
 
-var rr = 'https://i.ibb.co/x78n9mC/20230127-141609.jpg',
+var rr = 'https://i.ibb.co/x78n9mC/20230127-141609.jpg'
 await client.sendMessage(m.from,{image:{url:rr}, caption:rae},{quoted:m})
     }
 }
