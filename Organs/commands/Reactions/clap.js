81aa24a1017b2bcc7c8bc7c2156c620ddeb9a224
Aries-27 <101610365+Aries-27@users.noreply.github.com>
2023-01-28 21:17:53 +0530

    Update clap.js

--- Organs/commands/Reactions/clap.js
+++ Organs/commands/Reactions/clap.js
@@ -2,15 +2,15 @@ const { buffergif, fetchBuffer } = require('../../../lib/Function')
 const axios = require('axios')
 require ('../../../settings')
 module.exports={
-    name:"yeet",
-    alias:["yeet"],
-    usage:`${prefa}yeet`,
-    desc:"Sends a gif of yeet!",
+    name:"lick",
+    alias:["lickk"],
+    usage:`${prefa}lick`,
+    desc:"Sends a gif of licking!",
     category:"Reactions",
-    react:"🙂",
+    react:"😋",
 start:async(client,m,{command,nsfw,prefix,text})=>{
-  let assss = await axios.get ("https://api.waifu.pics/sfw/yeet")
-  var lol = '*You yeet at them🙂*'
+  let assss = await axios.get ("https://api.waifu.pics/sfw/lick")
+  var lol = '*You licked them😋*'
     var bobuff = await fetchBuffer(assss.data.url)
     var bogif = await buffergif(bobuff)
     await client.sendMessage(m.from,{video:bogif, caption:lol, gifPlayback:true },{quoted:m}).catch(err => {
