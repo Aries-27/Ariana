5e72f93749434170f0754a724a8cdb670ad04607
Aries-27 <101610365+Aries-27@users.noreply.github.com>
2023-01-28 16:00:19 +0530

    Update truth.js

--- Organs/commands/General/truth.js
+++ Organs/commands/General/truth.js
@@ -15,56 +15,15 @@ module.exports = {
         args
     }) => {
 let yup = [
-            " exam",
-                 "What is the worst thing you have ever done",
-                 "When was the last time you cried",
-                 "whom do you love the most among ur parents", 
-                 "do u sometimes put ur finger in ur nosetrilðŸ¤£", 
-                 "who was ur crush during the school days",
-                 "tell honestly, do u like any boy in this grup",
-                 "have you ever liked anyone? how long?",
-                 "do you have gf/bf','what is your biggest fear?",
-                 "have you ever liked someone and felt that person likes you too?",
-                 "What is the name of your ex boyfriend of your friend that you once liked quietly?",
-                 "ever did you steal your mothers money or your fathers money",
-                 "what makes you happy when you are sad",
-                 "do you like someone who is in this grup? if you then who?",
-                 "have you ever been cheated on by people?",
-                 "who is the most important person in your life",
-                 "what proud things did you get this year",
-                 "who is the person who can make you happy when u r sad",
-                 "who is the person who ever made you feel uncomfortable",
-                 "have you ever lied to your parents",
-                 "do you still like ur ex",
-                 "who do you like to play together with?",
-                 "have you ever stolen big thing in ur life? the reason why?",
-                 "Mention the incident that makes you hurt that you still remember",
-                 "what achievements have you got this year?",
-                 "what was your worst habit at school?",
-                 "do you love the bot creator Fantox?",
-                 "have you ever thought of taking revenge from ur teacher?",
-                 "do you like current prime minister of ur country",
-                 "you non veg or veg",
-                 "if you could be invisible, what is the first thing you would do",
-                 "what is a secret you kept from your parents",
-                 "Who is your secret crush",
-                 "whois the last person you creeped on social media",
-                 "If a genie granted you three wishes, what would you ask for",
-                 "What is your biggest regret",
-                 "What animal do you think you most look like",
-                 "How many selfies do you take a day",
-                 "What was your favorite childhood show",
-                 "if you could be a fictional character for a day, who would you choose",
-                 "whom do you text the most",
-                 "What is the biggest lie you ever told your parents",
-                 "Who is your celebrity crush",
-                 "Whats the strangest dream you have ever had",
-                 "do you play pubg, if you then send ur id number",
+            "https://i.ibb.co/x78n9mC/20230127-141609.jpg",
     ]
 let nezuko = await axios.get('https://api.github.com/repos/EternityBots/Nezuko')
 let rae = yup[Math.floor(Math.random() * yup.length)]
 
-var rr = "Test"
+let rr = "Test",
+"test1",
+    "test2",
+let rae = rr[Math.floor(Math.random() * rr.length)]
 await client.sendMessage(m.from,{image:{url:rae}, caption:rr},{quoted:m})
     }
 }
