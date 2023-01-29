9881087ec1d25fdff55fd5e7e9b974658d02f8cf
Aries-27 <101610365+Aries-27@users.noreply.github.com>
2023-01-29 09:29:16 +0530

    Update couple.js

--- Organs/commands/Fun/couple.js
+++ Organs/commands/Fun/couple.js
@@ -16,5 +16,5 @@ module.exports = {
     }) => {
 let anu = await fetchJson('https://raw.githubusercontent.com/iamriz7/kopel_/main/kopel.json')
          let random = anu[Math.floor(Math.random() * anu.length)]
-         Miku.sendMessage(m.chat, { image: { url: random.male }, caption: `For him...` }, { quoted: m })
-         Miku.sendMessage(m.chat, { image: { url: random.female }, caption: `For her...` }, { quoted: m })
+         await client.sendMessage(m.chat, { image: { url: random.male }, caption: `For him...` }, { quoted: m })
+         await client.sendMessage(m.chat, { image: { url: random.female }, caption: `For her...` }, { quoted: m })
