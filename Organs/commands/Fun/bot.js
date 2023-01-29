9f629067fa2f7f2791ef18b49fddcfa4a55426b0
Aries-27 <101610365+Aries-27@users.noreply.github.com>
2023-01-29 14:16:13 +0530

    Update bot.js

--- Organs/commands/Fun/bot.js
+++ Organs/commands/Fun/bot.js
@@ -12,12 +12,12 @@ module.exports={
     start:async(client,m,{command,prefix,text,args})=>{
 if (!text)  return m.reply(`Please provide me some text`)
 if (ggle == "none" || null)  return m.reply(`*❤️ Ask owner to give me an API key*`)
-await axios.get(`https://www.googleapis.com/customsearch/v1?q=${text}&key=${ggle}&cx=baf9bdb0c631236e5`)
+await axios.get(`http://api.brainshop.ai/get?bid=164782&key=fF16Q2FtvgIWMCvH&uid=[uid]&msg=${text}`)
 .then((res) => {
     if (res.status !== 200) return void m.reply(`🔍 Error: ${res.status}`);
     let result = ``;
     for (const item of res.data?.items) {
-        result += `*📒 Title* : ${item.title}\n*🔗Link* : ${item.link}\n*🎯 Description* : ${item.snippet}\n\n`;
+        result += `${text}`;
     }
 img = "https://i.ibb.co/26d2K4d/kindpng-209846.png"
  client.sendMessage(m.from,{image:{url:img},caption:result},{quoted:m})
