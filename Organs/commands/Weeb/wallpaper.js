cb5692af4399690f2c1eab14c6adcd3bb4c6c497
Aries-27 <101610365+Aries-27@users.noreply.github.com>
2023-01-29 07:57:21 +0530

    Update wallpaper.js

--- Organs/commands/Weeb/wallpaper.js
+++ Organs/commands/Weeb/wallpaper.js
@@ -41,7 +41,7 @@ module.exports = {
           url: wallpaper[i].image,
         },
         caption: `*Search term:* ${im[0]}`,
-        footer: `*©Eternity-Team*`,
+        footer: `*©Ronen-Bots*`,
         buttons: buttons,
         headerType: 4,
       };
