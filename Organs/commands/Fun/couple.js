7ae8652df3d14311d84a58086afdccf0e65b5b78
Aries-27 <101610365+Aries-27@users.noreply.github.com>
2023-01-29 09:27:58 +0530

    Update couple.js

--- Organs/commands/Fun/couple.js
+++ Organs/commands/Fun/couple.js
@@ -1,3 +1,19 @@
+
+const axios = require('axios')
+module.exports = {
+    name: "couple",
+    alias: ["pc"],
+    usage: `${prefa}ppcouple`,
+    desc: "Will send you matching couple dp",
+    react: "✅",
+    
+    category: "Fun",
+    start: async (client, m, {
+        command,
+        prefix,
+        text,
+        args
+    }) => {
 let anu = await fetchJson('https://raw.githubusercontent.com/iamriz7/kopel_/main/kopel.json')
          let random = anu[Math.floor(Math.random() * anu.length)]
          Miku.sendMessage(m.chat, { image: { url: random.male }, caption: `For him...` }, { quoted: m })
